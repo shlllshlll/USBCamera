d37c4bbe6e872a3f0c19058c9066e07e310ca61e
shlll <shlll7347@gmail.com>
2019-12-16 15:47:18 +0800

    chore: remove redundant files

--- libusbcamera/src/main/obj/local/x86/objs/UVCCamera/utilbase.o.d
+++
@@ -1,6 +0,0 @@
-./obj/local/x86/objs/UVCCamera/utilbase.o: jni/UVCCamera/utilbase.cpp \
-  jni/UVCCamera/../utilbase.h jni/UVCCamera/../localdefines.h
-
-jni/UVCCamera/../utilbase.h:
-
-jni/UVCCamera/../localdefines.h:
--- libusbcamera/src/main/obj/local/x86/objs/usb100_static/libusb/os/threads_posix.o.d
+++
@@ -1,5 +0,0 @@
-./obj/local/x86/objs/usb100_static/libusb/os/threads_posix.o: \
-  jni/libusb/android/jni/../../libusb/os/threads_posix.c \
-  jni/libusb/android/jni/../../libusb/os/threads_posix.h
-
-jni/libusb/android/jni/../../libusb/os/threads_posix.h:
